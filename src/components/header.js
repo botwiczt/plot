15f0ffb6213a5a52437f9517659f821fd798fca4
Tomek B <tomek.botwicz1@gmail.com>
2021-01-05 20:54:12 -0500

    Revert "fix down arrow (TEMP) to not appear further down page; add additional extra hover info."
    This reverts commit b5ed7c5cead9544212f6ba16106f6d2a5be714ee.

--- src/components/header.js
+++ src/components/header.js
@@ -151,29 +151,6 @@ const MobileTitle = styled.h1`
   }
 `;
 
-const ArrowImage = styled.img`
-  margin-left: auto;
-  margin-right: auto;
-  width: 6%;
-  position: absolute;
-  bottom: 0;
-  left: 47%;
-  animation: 1s Zoom linear infinite,
-  1s Always ease alternate infinite;
-  }
-  @keyframes Zoom {
-      100% { background-position: 100vw 0px; }
-  }
-
-  @keyframes Always {
-      100% { transform: scale(1.2);}
-  }
-  @media only screen and (max-width: 768px) {
-    left: 44%;
-    width: 12%;
-  }
-`;
-
 const WaitlistButton = styled.button`
   display: table;
   position: absolute;
@@ -250,7 +227,7 @@ const Header = ({ siteTitle }) => (
       <Title>plöt: the future of living.</Title>
       <Link to="/form">
       <WaitlistButton>join the waitlist</WaitlistButton></Link>
-      <a href="#down" aria-label="scroll down"><ArrowImage src="/downarrow.png"/></a>
+
   </DesktopHeader>
   <MobileHeader>
     <br/>
