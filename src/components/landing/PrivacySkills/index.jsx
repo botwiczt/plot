92ea36865cccea22195fee6306437f43e2984e62
Tomek B <tomek.botwicz1@gmail.com>
2021-02-25 14:26:37 -0500

    clarify privacy policy

--- src/components/landing/PrivacySkills/index.jsx
+++ src/components/landing/PrivacySkills/index.jsx
@@ -14,10 +14,10 @@ export const PrivacySkills = () => {
       <Details theme={theme}>
       <h1>privacy policy:</h1>
       <p><b>last updated february 25th, 2021
-<br/><br/>> we do not automatically collect any personal information from site visitors.
-<br/>> we collect your email if entered into the waitlist.
-<br/>> we store all collected emails along with a timestamp of collection on a secure server.
-<br/>> we do not use any analytical software.</b></p>
+<br/><br/>> we do not automatically collect any personal information.
+<br/>> we collect any email entered into the waitlist.
+<br/>> we store collected emails along with a collection timestamp on a secure server.
+<br/>> we do not use analytical or tracking software.</b></p>
       </Details>
       </SkillsWrapper>
     </Wrapper>
