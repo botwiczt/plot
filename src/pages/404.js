76eedb4549a12bf151280d83f657c262ca7ef844
Tomek B <46640406+botwiczt@users.noreply.github.com>
2021-05-18 23:09:10 -0400

    redesign 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,13 @@
 import React, { useContext } from 'react';
 import { Link } from "gatsby"
-import { SidePageLayout, SEO } from 'components/common';
+import { SidePageLayout, SEO, Button } from 'components/common';
 import { ThemeContext } from 'providers/ThemeProvider';
 import styled from "styled-components";
 import logo from 'assets/illustrations/logo.svg';
+
+const Center = styled.div`
+  text-align: center;
+`;
 
 const Brand = styled.img`
   display: block;
@@ -16,32 +20,6 @@ const Brand = styled.img`
   }
 `;
 
-const ErrorContent = styled.p`
-  text-align: center;
-`;
-
-const Button = styled.button`
-  cursor: pointer;
-  font-size: 20px;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  padding-left: 20px;
-  padding-right: 20px;
-  border: none;
-  border-radius: 10px;
-  transition: all 0.5s ease;
-  background: ${({ theme }) => (theme === 'dark' ? '#FFF' : '#000')};
-  color: ${({ theme }) => (theme === 'dark' ? '#000' : '#FFF')};
-
-  :hover {
-    box-shadow: inset 0 0 100px 100px #444d56;
-    transform: scale(1.15);
-  }
-  @media only screen and (max-width: 672px) {
-      border-radius: 3px;
-    }
-`;
-
 export default () => {
 
   const { theme } = useContext(ThemeContext);
@@ -51,12 +29,13 @@ export default () => {
     <SEO title="404: Not found" location="/404" />
     <Brand as={Link} to="/" theme={theme}>
       <img src={logo} alt="company logo" />
-    </Brand>
-    <ErrorContent>
+    </Brand><br/>
+    <Center>
     <h1>404: not found</h1>
     <p>this page does not exist.</p>
-    <Link to="/"><Button theme={theme}>return home</Button></Link>
-    </ErrorContent>
+    <Link to="/">
+    <Button theme={theme}>return home</Button></Link>
+    </Center>
   </SidePageLayout>
 );
 };
