025140b141aace30648fe284c375f92f944e33c6
Tomek B <46640406+botwiczt@users.noreply.github.com>
2021-05-21 11:53:40 -0400

    significantly simplify generic header

--- src/components/theme/GenericHeader/index.jsx
+++ src/components/theme/GenericHeader/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import Navbar from './Navbar';
+import React, { useContext } from 'react';
+import { ThemeContext } from 'providers/ThemeProvider';
+import { Container } from 'components/common';
+import { Wrapper, Brand, Wrapper2, NavButton } from './styles';
+import logo from 'assets/illustrations/logo.svg';
+import { Link } from 'gatsby';
+import back from 'assets/illustrations/leftarrow.svg';
 
 export const GenericHeader = () => {
+  const { theme } = useContext(ThemeContext);
 
   return (
-      <Navbar />
+    <Wrapper as={Container}>
+      <Wrapper2 theme={theme}>
+        <Link to="/" theme={theme}>
+          <NavButton theme={theme} src={back}/>
+        </Link>
+      </Wrapper2>
+      <Brand theme={theme}>
+        <img src={logo} alt="company logo" />
+      </Brand>
+    </Wrapper>
   );
 };
+
+export default GenericHeader;
--- src/components/theme/GenericHeader/styles.js
+++ src/components/theme/GenericHeader/styles.js
@@ -6,6 +6,25 @@ export const Wrapper = styled.div`
   justify-content: space-between;
 `;
 
+export const Wrapper2 = styled.div`
+ margin-top: 5%;
+`;
+
+export const NavButton = styled.img`
+  width: 6vw;
+  cursor: pointer;
+  padding: 10px;
+  transition: all 0.5s ease;
+  filter: ${({ theme }) => (theme === 'dark' ? 'invert(1)' : 'invert(0)')};
+  :hover {
+    transform: scale(1.2);
+    opacity: 0.45;
+  }
+  @media (max-width: 960px) {
+    width: 12vw;
+  }
+`;
+
 export const Brand = styled.a`
   color: ${({ theme }) => (theme === 'light' ? '#000' : '#fff')};
   width: 25%;
